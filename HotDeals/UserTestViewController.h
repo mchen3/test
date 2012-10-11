9568bd48d059a08a1ebba6cf875779219134cce9
Mike Chen <tap135@hotmail.com>
2012-10-11 13:20:07 -0400

    Added TestUserViewController

---
+++ HotDeals/UserTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserTestViewController.h
+//  HotDeals
+//
+//  Created by Mike Chen on 10/11/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserTestViewController : UIViewController
+
+@end
---
+++ HotDeals/UserTestViewController.m
@@ -0,0 +1,44 @@
+//
+//  UserTestViewController.m
+//  HotDeals
+//
+//  Created by Mike Chen on 10/11/12.
+//
+//
+
+#import "UserTestViewController.h"
+
+@interface UserTestViewController ()
+
+@end
+
+@implementation UserTestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
